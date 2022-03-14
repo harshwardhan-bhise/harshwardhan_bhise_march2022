723ce9136e8a099af08262b596699905c8b5d5b6
harshwardhan-bhise <101185551+harshwardhan-bhise@users.noreply.github.com>
2022-03-15 00:35:13 +0530

    Add files via upload

---
+++ Assingnment3/NumPat16.java
@@ -0,0 +1,31 @@
+/*class NumPat16{
+
+
+public static void main(String args[])
+{
+	for(int i=5;i>=1;i--)
+	{
+		for(int j=5;j>=i;j--)
+		{
+			System.out.print(j+" ");
+		}
+		System.out.println();
+	}
+	
+}}*/
+
+class NumPat16{
+
+
+public static void main(String args[])
+{
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=5;j>=6-i;j--)
+		{
+			System.out.print(6-j+" ");
+		}
+		System.out.println();
+	}
+	
+}}
---
+++ Assingnment3/Star8.java
@@ -0,0 +1,38 @@
+class Star8{
+
+
+public static void main(String args[])
+{
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+	
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=4;j>=i;j--)
+			{
+				System.out.print("*");
+			}
+		System.out.println();
+	}
+}}
+
+
+/*
+public static void main1(String args[])
+{
+	for(int i=1;i<=4;i++)
+	{
+		for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+		System.out.println();
+	}
+}
+}*/
